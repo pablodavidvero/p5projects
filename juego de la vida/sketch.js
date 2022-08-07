6a25b4e624c194f74953196fd7d2442637794826
Pablo Romero HP <pablo.david.vero@gmail.com>
2022-08-07 11:46:53 -0600

    cambios en generaciones

--- juego de la vida/sketch.js
+++ juego de la vida/sketch.js
@@ -4,8 +4,9 @@ let anchuraDashboardInfo = 200;
 let alturaTotal = 700;
 let tamanioCasilla = 25;
 let conteoCelulas = 0;
-let maxCelulasAleatorias = 10;
+let maxCelulasAleatorias = 40;
 let pausado = true;
+let generacion = 0;
 
 let cant_x = (anchuraTotal - anchuraDashboardInfo) / tamanioCasilla;
 let cant_y = alturaTotal / tamanioCasilla;
@@ -16,20 +17,37 @@ function setup() {
 	frameRate(framesPerSecond);
 	createCanvas(anchuraTotal, alturaTotal);
 	inicializarCelulasVacias(celulasActuales);
-	crearSemillaInicialAleatoria();
+	//crearSemillaInicialAleatoria();
 }
 
 function keyPressed() {
 	if (keyCode === ENTER) {
-		pausado = false;
+		pausado = !pausado;
 	}
 }
+
+function mouseClicked() {
+	console.log(`mouse posxy -> ${mouseX}, ${mouseY}`);
+	if(mouseX < 0 || mouseY < 0)
+		return;
+	if(mouseX > (anchuraTotal - anchuraDashboardInfo))
+		return;
+	let posx = int(mouseX / tamanioCasilla)+1;
+	let posy = int(mouseY / tamanioCasilla);
+	let postemp = getPosLineal(posx, posy);
+	if(!(postemp > 0)) {
+		return;
+	}
+	console.log(`postemp -> ${postemp}`);
+	celulasActuales[postemp] = celulasActuales[postemp] == 0 ? 1 : 0;
+  }
 
 function draw() {
 	background(0);
 	dibujarMatrixVacia();
 	dibujarCelulas();
 	dibujarDashboardInfo();
+	generarNuevaGeneracion ();
 }
 
 function generarNuevaGeneracion () {
@@ -39,14 +57,58 @@ function generarNuevaGeneracion () {
 	let nuevaGeneracion = [];
 	inicializarCelulasVacias(nuevaGeneracion);
 	celulasActuales.forEach((activa, pos) => {
-		let cant_vecinas_vivas = 0;
+		// if(activa == 0)
+		// 	return;
+		let cant_vecinas_vivas = obtenerCantCelulasVecinasVivas(pos);
 		//primera regla
-		if(activa == 1 && (cant_vecinas_vivas = 2 || cant_vecinas_vivas == 3 ))
+		if(activa == 1 && (cant_vecinas_vivas == 2 || cant_vecinas_vivas == 3 ))
 			nuevaGeneracion[pos] = 1;
 		//segunda regla
 		if(activa == 0 && (cant_vecinas_vivas == 3 ))
 			nuevaGeneracion[pos] = 1;
+		//tercera regla
+		if(activa == 1 && (cant_vecinas_vivas > 3 ))
+			nuevaGeneracion[pos] = 0;
+		if(activa == 1 && cant_vecinas_vivas < 2)
+			nuevaGeneracion[pos] = 0;
 	});
+	generacion++;
+	// pausado = true;
+	// return;
+	celulasActuales = nuevaGeneracion;
+}
+
+function obtenerCantCelulasVecinasVivas(pos) {
+	let cant = 0;
+	let posy = int((pos-1) / cant_x) + 1;
+	let posx = (pos-1) % cant_x + 1; 
+	let postemp
+	postemp = getPosLineal(posx + 1, posy);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx + 1, posy + 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx, posy + 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx - 1, posy);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx - 1, posy - 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx, posy - 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx + 1, posy - 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	postemp = getPosLineal(posx - 1, posy + 1);
+	if(postemp > 0 && celulasActuales[postemp] > 0)
+		cant++;
+	console.log(`cant de celulas vecinas vivas de pos ${pos} -> posxy (${posx}, ${posy}) : ${cant}`);
+	return cant;	
 }
 
 function dibujarMatrixVacia() {
@@ -106,14 +168,16 @@ function dibujarDashboardInfo() {
 	stroke(255);
 	line((anchuraTotal-anchuraDashboardInfo), 0, (anchuraTotal-anchuraDashboardInfo), alturaTotal);
 	textSize(16);
-	text(`Generación: ${frameCount}`, (anchuraTotal-anchuraDashboardInfo) + desfaceIzq, tamanioFila*filaActual++);
+	text(`Generación: ${generacion}`, (anchuraTotal-anchuraDashboardInfo) + desfaceIzq, tamanioFila*filaActual++);
 	text(`Células: ${conteoCelulas}`, (anchuraTotal-anchuraDashboardInfo) + desfaceIzq, tamanioFila*filaActual++);
 	text(`Pausado: ${pausado ? 'si' : 'no'}`, (anchuraTotal-anchuraDashboardInfo) + desfaceIzq, tamanioFila*filaActual++);
+	text(`Matriz: ${cant_x}x${cant_y}`, (anchuraTotal-anchuraDashboardInfo) + desfaceIzq, tamanioFila*filaActual++);
 }
 
 function getPosLineal(posx, posy) {
 	//matriz 3*3 = linea 9 
 	//pos x, y -> 2,2 -> 5 -> 3*(2-1) + 2
+	//console.log(`${posx}, ${posy}`);
 	if(posx < 1 || posy < 1)
 		return -1;
 	if(posx > (cant_x) || posy > (cant_y))
